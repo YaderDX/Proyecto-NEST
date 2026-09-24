8082c35dd7b0a100dea2dcfab355e65b1cb6ddc5
YaderDX <yaderoviedo777@gmail.com>
2026-09-24 07:20:07 -0600

    Actualizacion de Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../prisma/prisma.module.js';
 
 @Module({
+
+  imports: [
+  PrismaModule,
+  JwtModule.register({
+    secret: 'your-secret-key',
+    signOptions: {expiresIn: '1h'},
+  }),
+
+],
+
   providers: [AuthService],
   controllers: [AuthController]
 })
