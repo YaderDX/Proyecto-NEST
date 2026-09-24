fd476d43be745f9fcf831263101fc42b23b38d02
YaderDX <yaderoviedo777@gmail.com>
2026-09-24 04:42:38 -0600

    Cambios de Swagger y Agregacion de Usuarios

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service.js';
+import { UsersController } from './users.controller.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [PrismaModule]
+})
+export class UsersModule {}
